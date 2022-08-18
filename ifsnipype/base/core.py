85ce81696984c06b4b8410837502b55df441db88
Oscar Esteban <code@oscaresteban.es>
2022-08-18 07:22:48 +0200

    enh: keep reducing base interface boilerplate
    Removes the check methods

--- ifsnipype/base/core.py
+++ ifsnipype/base/core.py
@@ -84,29 +84,22 @@ class BaseInterface(_Interface):
     _redirect_x = False
     _references = []
 
-    def __init__(
-        self, from_file=None, ignore_exception=False, **inputs
-    ):
+    def __init__(self, **inputs):
         if not self.input_spec:
             raise Exception("No input_spec in class: %s" % self.__class__.__name__)
 
         # Create input spec, disable any defaults that are unavailable due to
         # version, and then apply the inputs that were passed.
         self.inputs = self.input_spec()
-        unavailable_traits = self._check_version_requirements(
-            self.inputs, permissive=True
-        )
-        if unavailable_traits:
-            self.inputs.trait_set(**{k: Undefined for k in unavailable_traits})
+
+        # TODO: Consider version checking
+        # unavailable_traits = self._check_version_requirements(
+        #     self.inputs, permissive=True
+        # )
+        # if unavailable_traits:
+        #     self.inputs.trait_set(**{k: Undefined for k in unavailable_traits})
         self.inputs.trait_set(**inputs)
 
-        self.ignore_exception = ignore_exception
-
-        if from_file is not None:
-            self.load_inputs_from_json(from_file, overwrite=True)
-
-            for name, value in list(inputs.items()):
-                setattr(self.inputs, name, value)
 
     def _outputs(self):
         """Returns a bunch containing output fields for the class"""
@@ -115,144 +108,6 @@ class BaseInterface(_Interface):
             outputs = self.output_spec()
 
         return outputs
-
-    def _check_requires(self, spec, name, value):
-        """check if required inputs are satisfied"""
-        if spec.requires:
-            values = [
-                not isdefined(getattr(self.inputs, field)) for field in spec.requires
-            ]
-            if any(values) and isdefined(value):
-                if len(values) > 1:
-                    fmt = (
-                        "%s requires values for inputs %s because '%s' is set. "
-                        "For a list of required inputs, see %s.help()"
-                    )
-                else:
-                    fmt = (
-                        "%s requires a value for input %s because '%s' is set. "
-                        "For a list of required inputs, see %s.help()"
-                    )
-                msg = fmt % (
-                    self.__class__.__name__,
-                    ", ".join("'%s'" % req for req in spec.requires),
-                    name,
-                    self.__class__.__name__,
-                )
-                raise ValueError(msg)
-
-    def _check_xor(self, spec, name, value):
-        """check if mutually exclusive inputs are satisfied"""
-        if spec.xor:
-            values = [isdefined(getattr(self.inputs, field)) for field in spec.xor]
-            if not any(values) and not isdefined(value):
-                msg = (
-                    "%s requires a value for one of the inputs '%s'. "
-                    "For a list of required inputs, see %s.help()"
-                    % (
-                        self.__class__.__name__,
-                        ", ".join(spec.xor),
-                        self.__class__.__name__,
-                    )
-                )
-                raise ValueError(msg)
-
-    def _check_mandatory_inputs(self):
-        """Raises an exception if a mandatory input is Undefined"""
-        for name, spec in list(self.inputs.traits(mandatory=True).items()):
-            value = getattr(self.inputs, name)
-            self._check_xor(spec, name, value)
-            if not isdefined(value) and spec.xor is None:
-                msg = (
-                    "%s requires a value for input '%s'. "
-                    "For a list of required inputs, see %s.help()"
-                    % (self.__class__.__name__, name, self.__class__.__name__)
-                )
-                raise ValueError(msg)
-            if isdefined(value):
-                self._check_requires(spec, name, value)
-        for name, spec in list(
-            self.inputs.traits(mandatory=None, transient=None).items()
-        ):
-            self._check_requires(spec, name, getattr(self.inputs, name))
-
-    def _check_version_requirements(self, trait_object, permissive=False):
-        """Raises an exception on version mismatch
-
-        Set the ``permissive`` attribute to True to suppress warnings and exceptions.
-        This is currently only used in __init__ to silently identify unavailable
-        traits.
-        """
-        if not self.version:
-            # TODO: Raise error if versions are to be enforced and this one is unknown
-            # if str2bool(config.get("execution", "stop_on_unknown_version")):
-            #     raise ValueError(
-            #         "Interface %s has no version information" % self.__class__.__name__
-            #     )
-            return []
-
-        from nipype import config, LooseVersion
-
-        unavailable_traits = []
-        # check minimum version
-        check = dict(min_ver=lambda t: t is not None)
-        names = trait_object.trait_names(**check)
-
-        if names and self.version:
-            version = LooseVersion(str(self.version))
-            for name in names:
-                min_ver = LooseVersion(str(trait_object.traits()[name].min_ver))
-                try:
-                    too_old = min_ver > version
-                except TypeError as err:
-                    msg = (
-                        f"Nipype cannot validate the package version {version!r} for "
-                        f"{self.__class__.__name__}. Trait {name} requires version >={min_ver}."
-                    )
-                    if not permissive:
-                        _iflogger.warning(f"{msg}. Please verify validity.")
-                    if config.getboolean("execution", "stop_on_unknown_version"):
-                        raise ValueError(msg) from err
-                    continue
-                if too_old:
-                    unavailable_traits.append(name)
-                    if not isdefined(getattr(trait_object, name)):
-                        continue
-                    if not permissive:
-                        raise Exception(
-                            "Trait %s (%s) (version %s < required %s)"
-                            % (name, self.__class__.__name__, version, min_ver)
-                        )
-
-        # check maximum version
-        check = dict(max_ver=lambda t: t is not None)
-        names = trait_object.trait_names(**check)
-        if names and self.version:
-            version = LooseVersion(str(self.version))
-            for name in names:
-                max_ver = LooseVersion(str(trait_object.traits()[name].max_ver))
-                try:
-                    too_new = max_ver < version
-                except TypeError as err:
-                    msg = (
-                        f"Nipype cannot validate the package version {version!r} for "
-                        f"{self.__class__.__name__}. Trait {name} requires version <={max_ver}."
-                    )
-                    if not permissive:
-                        _iflogger.warning(f"{msg}. Please verify validity.")
-                    if config.getboolean("execution", "stop_on_unknown_version"):
-                        raise ValueError(msg) from err
-                    continue
-                if too_new:
-                    unavailable_traits.append(name)
-                    if not isdefined(getattr(trait_object, name)):
-                        continue
-                    if not permissive:
-                        raise Exception(
-                            "Trait %s (%s) (version %s > required %s)"
-                            % (name, self.__class__.__name__, version, max_ver)
-                        )
-        return unavailable_traits
 
     def _run_interface(self, runtime):
         """Core function that executes interface"""
@@ -285,16 +140,15 @@ class BaseInterface(_Interface):
         """
         # TODO: Remove nipype dependency on indirectory
         from nipype.utils.filemanip import indirectory
-        rtc = RuntimeContext(
-            ignore_exception=ignore_exception
-            if ignore_exception is not None
-            else self.ignore_exception,
-        )
+        rtc = RuntimeContext(ignore_exception=bool(ignore_exception))
 
         with indirectory(cwd or os.getcwd()):
             self.inputs.trait_set(**inputs)
-        self._check_mandatory_inputs()
-        self._check_version_requirements(self.inputs)
+        # TODO: enable check
+        # self._check_mandatory_inputs()
+
+        # TODO: Version checking?
+        # self._check_version_requirements(self.inputs)
 
         with rtc(self, cwd=cwd, redirect_x=self._redirect_x) as runtime:
 
@@ -370,35 +224,9 @@ Output trait(s) %s not available in version %s of interface %s.\
                 raise error
         return outputs
 
-    @property
-    def version(self):
-        return self._version
-
-    def load_inputs_from_json(self, json_file, overwrite=True):
-        """
-        A convenient way to load pre-set inputs from a JSON file.
-        """
-
-        with open(json_file) as fhandle:
-            inputs_dict = json.load(fhandle)
-
-        def_inputs = []
-        if not overwrite:
-            def_inputs = list(self.inputs.get_traitsfree().keys())
-
-        new_inputs = list(set(list(inputs_dict.keys())) - set(def_inputs))
-        for key in new_inputs:
-            if hasattr(self.inputs, key):
-                setattr(self.inputs, key, inputs_dict[key])
-
-    def save_inputs_to_json(self, json_file):
-        """
-        A convenient way to save current inputs to a JSON file.
-        """
-        inputs = self.inputs.get_traitsfree()
-        _iflogger.debug("saving inputs %s", inputs)
-        with open(json_file, "w") as fhandle:
-            json.dump(inputs, fhandle, indent=4, ensure_ascii=False)
+    # @property
+    # def version(self):
+    #     return self._version
 
     def _pre_run_hook(self, runtime):
         """
@@ -463,8 +291,8 @@ class SimpleInterface(BaseInterface):
 
     """
 
-    def __init__(self, from_file=None, **inputs):
-        super().__init__(from_file=from_file, **inputs)
+    def __init__(self, **inputs):
+        super().__init__(**inputs)
         self._results = {}
 
     def _list_outputs(self):
